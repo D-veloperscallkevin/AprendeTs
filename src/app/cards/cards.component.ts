1d41a306613683a0b03c126962052ff3159fe5e5
Kevin Aguilar <kevin.aguilaru@gmail.com>
2023-02-25 02:40:39 -0600

    arreglando pagina
    con la info rellena

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -8,45 +8,34 @@ import { Component } from '@angular/core';
 export class CardsComponent {
 info:Array<any>=[
   {
-    title: 'possimus corporis?',
+    title: 'Boolean',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'Las variables booleanas se almacenan como números de 16 bits, aunque solo pueden tener los valores True o False. ',
   },
   {
-    title: 'possimus corporis?',
+    title: 'Array',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'Los arrays son variables estructuradas, donde cada elemento se almacena de forma consecutiva en memoria',
   },
   {
-    title: 'possimus corporis?',
+    title: 'Tuple',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'permiten expresar un arreglo con un número fijo conocido de elementos, pero que no requieren ser los mismos.',
   },
   {
-    title: 'possimus corporis?',
+    title: 'Enum',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'Envolve en un objeto determinadas propiedades como constantes estáticas con valores específicos del negocio',
   },
   {
-    title: 'possimus corporis?',
+    title: 'Any',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'Es la via de escape de TypeScript. Puedes usar any para declarar que una sección de tu código es dinámica y parecida a JavaScript.',
   },
   {
-    title: 'possimus corporis?',
+    title: 'Never',
     subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
-  },
-  {
-    title: 'possimus corporis?',
-    subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
-  },
-  {
-    title: 'possimus corporis?',
-    subtitle: 'culpa, possimus corporis?',
-    info: 'eiciendis repudiandae accusamus ea voluptatum aut molestiae, autem culpa, possimus corporis?',
+    info: 'Utilizar una función que retorna `never` permite agregar código adicional en lugares donde no debería ser posible.',
   }
-
 ]
 }
